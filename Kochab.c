52ce2433069777e50bc372ed5e1c0aac69a4468e
Niki Biggs <32183360+RatzKlaw@users.noreply.github.com>
2017-09-27 23:26:33 -0600

    Added system level random generation
    alot has been done, the two big ones are:
    the implementation of random system map generation
    -this uses a switch and values between 0 and 100, this allows regulation of spawn rates of different items in the system(there are currently 10)
    The second is the conversion of the system and zone lists to function similarly to a hash table (they don't use a hash, just the coordinates converged into a single variable) along with progress on functions that will see use with the editing of maps
    
    the minor changes are mainly simple bug resolutions, and removing statements used in debugging

--- Kochab.c
+++ Kochab.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
+#include <stdint.h>
 
 //---------------|| Map datatypes			 	||----------
 
@@ -26,7 +27,7 @@ typedef struct galmap 					//struct for galaxy map, currently seems unnessecary,
 
 typedef struct sysmap
 {
-	int sm_coord[2];				//this is the coordinate of a system/zone in (x,y) coordinate format index 0 is x, 1 is y
+	int16_t scompress;				//this holds a compressed value of the user selected coordinates, it is being used as a way to better understand the concept of hash tables(even without a hash algorithm being involved), and change from comparing a array with two indexs to comparing just a single value
 	char sysmapc[6][6];			//this is a sub map, specifically zones within a system
 	char s_descrip[255];			//description of the currently selected system
 	struct sysmap *s_nextnode;		
@@ -35,7 +36,7 @@ typedef struct sysmap
 
 typedef struct zonemap
 {
-	int z_coord[2];				//same as sysmap's sm_coord
+	int32_t zcompress;				//same as sysmap's sm_coord but function is coordhash32
 	char z_descrip[255];
 	struct zonemap *z_nextnode;
 	struct zonemap *z_prevnode;
@@ -70,11 +71,18 @@ void gmapcreate(struct galmap **,struct sysmap **,struct zonemap **);//the point
 void blankmap(struct galmap **,struct sysmap **,struct zonemap **);	//blank map generation
 void randmap(struct galmap **,struct sysmap **,struct zonemap **, int);//Map creation populated procedurally the integer is used when the function is called due to invalid input, 0 means cold run, 1 means it's been run once already
 
-//The node creation functions will accept a target node and interger ranging from 1-3 1: delete current 2: pre insert 3: post insert
-struct galmap * gnodemanip(struct galmap *, int);	//node creation: galaxy map
-struct sysmap * snodemanip(struct sysmap *, int);	//node creation: system map
-struct zonemap * znodemanip(struct zonemap *, int);	//node creation: zone map
+//The node creation functions
+struct galmap * gnodecre(struct galmap **);	//node creation: galaxy map
+void snodecre(struct sysmap **,int,int);	//node creation: system map
+struct zonemap * znodecre(struct zonemap **, int32_t);	//node creation: zone map
 
+//The node Deletion functions 0: good/node deleted 1:node not found
+int sysnodedel(int16_t);
+int znodedel(zonemap_dt **);
+
+//coordinate hash functions
+int16_t coordcompress16(int,int);
+int32_t coordcompress32(int,int,int,int);
 
 
 //---------------|| Trash collection and debug functions 	||------------
@@ -88,6 +96,10 @@ void originprint();
 
 int main()
 {
+/*	int hashprotozoa = 1;
+	hashprotozoa= hashprotozoa << 2;
+	printf("hashprotozoa is: %d",hashprotozoa);
+	system("pause");*/
 	getmenu();	//Begin accepting user data
 	//system("pause");
 }
@@ -115,14 +127,15 @@ void blankmap(galmap_dt **ginitial, struct sysmap **sinitial, struct zonemap **z
 	{
 		for(int j = 0;j<6;j++)
 		{
-			(*sinitial)->sysmapc[i][j] = (j+48+i);//currently unknown about what format data in sysmap will be, leave this as a placeholder
+			(*sinitial)->sysmapc[i][j] = 0;//currently unknown about what format data in sysmap will be, leave this as a placeholder
 		}
 	}
 	*zinitial = (struct zonemap *) malloc(sizeof(struct zonemap));	//allocate memory and assign pointer
 	(*zinitial)->z_nextnode = NULL;	//set previous nodes address
 	(*zinitial)->z_prevnode = NULL;	//set next nodes address
-	(*zinitial)->z_coord[0] = 1;//debug
-	(*zinitial)->z_coord[1] = 1;//debug
+	galorigin = (*ginitial);
+	sysorigin = (*sinitial);
+	zorigin = (*zinitial);
 }
 
 void randmap(galmap_dt **ginitial, sysmap_dt **sinitial, zonemap_dt **zinitial, int reused)
@@ -130,8 +143,9 @@ void randmap(galmap_dt **ginitial, sysmap_dt **sinitial, zonemap_dt **zinitial, 
 	if(reused == 0){
 	blankmap(*&ginitial, *&sinitial, *&zinitial); system("CLS");}//to keep the program slim, randmap will take blankmaps output and expand upon it
 	char keyselect;//variable for user input
+	int isstar = 1;
 	int randinhib = 50;//shortened rand inhibitor, it is made to act as a dynamic gate for the creation of galmap level systems, each time a system is created randinhib will increase, decreasing the chances of antoher system generating
-	int areabias = 0;// a bias to be used to modify randinhib, it's value changes depending on how many entities exist within a +-2 vertical/horizontal and +-1 diagonal diamond around select point system will only reachout one in all directions 
+	int areabias = 0;// a bias to modify what object is selected for placement at the system level: (P)lanet, (S)tar, (H)arbor, (A)nomoly, (F)leet/Flotilla, (D)ebris, (O)ther (may not be used for random), (B)attle, (N)atural fuel, b(E)acon
 	int x_initial = 2, y_initial = 2, randkey = time(NULL);// randkey is a variable for the random output, at the start fo the function it will be used to store the seed for random which will be applied later
 	printf("Would you like to use a random seed or your own seed for the random generator? (Y/N)\n");
 	keyselect = getchar();
@@ -167,26 +181,102 @@ void randmap(galmap_dt **ginitial, sysmap_dt **sinitial, zonemap_dt **zinitial, 
 				for(int j = y-2; (y-2) <= j <= (y+2) && j < 10; j++)// the and is there to prevent out of bounds array index
 				{
 					if(i < 0)// these two ifs are to prevent the program trying to access memory outside of the galaxy array
-					{i = 0;randinhib += abs(i)*2;}// randinhib offset to balance not checking multiple indexes that don't exist
+					{randinhib += abs(i)*2;i = 0;}// randinhib offset to balance not checking multiple indexes that don't exist
 					if(j < 0)
-					{j = 0;randinhib += abs(j)*2;}
-					if((*ginitial)->gmapover[x][y] != 0)
+					{randinhib += abs(j)*2;j = 0;}
+					if((*ginitial)->gmapover[i][j] != 0)
 					{
-						randinhib += 2;//+2  due to randinhib starting at 50, with a +-2 x/y search area, it searches through 25-1(for the start point) indexxes, in a total population scenerio, rand inhib would be 100, leaving 0% chance of spawn beacause t would be 50/25 populated indexxes so every populated index increasses spawn chance by 2%.
+						randinhib += 3;//+2  due to randinhib starting at 50, with a +-2 x/y search area, it searches through 25-1(for the start point) indexxes, in a total population scenerio, rand inhib would be 100, leaving 0% chance of spawn beacause t would be 50/25 populated indexxes so every populated index increasses spawn chance by 2%.
 						}
 				}
 			}
-			/*
-			this area will contain the code to generate the chance of a system spawning, and setting it in the array, and then clearing the inhibitor variables and manipulating any other data
-			*/
-			randkey = rand()%100;
-			if(randkey > randinhib)
+
+			randkey = rand()%100;//chance of spawn
+			if(randkey > randinhib)//compare to the inhibitor
 			{
-				(*ginitial)->gmapover[x][y] = 'S';
+				(*ginitial)->gmapover[x][y] = 'S';//set a index to represent a system
 			}
 			randinhib = 50;
 		}
 	}
+	for(int i = 0;i < 10; i++)
+	{for(int j = 0; j< 10;j++)
+		{
+			if((*ginitial)->gmapover[i][j] == 'S')
+			{
+				for(int x=0; x < 6; x++)//the X/Y only loops are for populating the map, everything within the y forloop is related to system generation
+				{
+					for(int y = 0; y < 6; y++)
+					{for(int k = x-1; (x-1) <= k <= (x+1) && k < 6; k++)//due to the map being 10x10 the generation will check in a two layer square around the selected point for other objects within the area and will change the inhibitor variables to make it more difficult for a system to spawn for every system within the area
+						{for(int l = y-1; (y-1) <= l <= (y+1) && l < 6; l++)// the and is there to prevent out of bounds array index
+							{						
+								if(k < 0)
+								{areabias += abs(k)*1;randinhib += abs(k)*5;k = 0;}// randinhib offset to balance not checking multiple indexes that don't exist
+							if(l < 0)
+								{areabias += abs(l)*1;randinhib += abs(l)*5;l = 0;}
+							if((*sinitial)->sysmapc[k][l] != 0)
+								{randinhib += 5;}
+							//printf("\nK is: %d\t L is: %d\t randinhib is: %d",k,l,randinhib);
+							}
+						}
+						randkey = rand()%100;//chance of spawn
+						if(randkey > randinhib)//compare to the inhibitor
+						{
+						(*sinitial)->scompress = coordcompress16(x,y);
+						randkey = rand()%100;//chance of spawn
+						if(isstar != -1){randkey += isstar;}
+						switch(1)//system legend: (P)lanet, (S)tar, (H)arbor, (A)nomoly, (F)leet/Flotilla, (D)ebris, (O)ther (may not be used for random), (B)attle, (N)atural fuel, b(E)acon
+						{
+							case 1://this will work similar to a nopsled, it will go until it finds the instruction to do something else allowing me to add minimum percentages to the switch
+								(*sinitial)->sysmapc[x][y] = 'S';//percent to spawn: 5%
+								if(randkey >=99){isstar = -1;break;}
+							case 2:
+								isstar +=4;
+								(*sinitial)->sysmapc[x][y] = 'P';//10%
+								if(randkey >= 85){break;}
+							case 3:
+								(*sinitial)->sysmapc[x][y] = 'H';//5%
+								if(randkey >= 80){break;}
+							case 4:
+								(*sinitial)->sysmapc[x][y] = 'A';//20%
+								if(randkey >= 60){break;}
+							case 5:
+								(*sinitial)->sysmapc[x][y] = 'F';//10%
+								if(randkey >= 50){break;}
+							case 6:
+								(*sinitial)->sysmapc[x][y] = 'D';//10%
+								if(randkey >= 40){break;}
+							case 7:
+								(*sinitial)->sysmapc[x][y] = 'O';//10%
+								if(randkey >= 30){break;}
+							case 8:
+								(*sinitial)->sysmapc[x][y] = 'B';//5%
+								if(randkey >= 25){break;}
+							case 9:
+								(*sinitial)->sysmapc[x][y] = 'N';//10%
+								if(randkey >= 15){break;}
+							case 10:
+								(*sinitial)->sysmapc[x][y] = 'E';//15%
+								if(randkey < 0 || randkey > 100){//this represents the default case which can't co-exist with the nature of this switch
+								printf("\nCritical error in system map level generation...\nError cause by input: %d\nPress any key to activate exit timer....\n",randkey);
+								getchar();
+								uexit();}
+								break;}
+						}
+						randinhib = 50;
+						areabias = 0;
+					}
+				}isstar = 0;
+				snodecre(*&sinitial,i,j);
+			}
+	}}
+	(*sinitial)->scompress = -132;
+	int hold = sysnodedel(-132);
+	while ((*sinitial)->s_prevnode != NULL)
+	{
+		(*sinitial) = (*sinitial)->s_prevnode;
+	}
+	
 }
 
 void gmapcreate(galmap_dt **ginitial, sysmap_dt **sinitial, zonemap_dt **zinitial)
@@ -215,6 +305,84 @@ void gmapcreate(galmap_dt **ginitial, sysmap_dt **sinitial, zonemap_dt **zinitia
 		default:
 			printf("\nUnknown input Error...\n");
 	}
+}
+
+void snodecre(sysmap_dt ** sinitial, int xin, int yin)
+{
+	int16_t Hkey = coordcompress16(xin,yin);//get hash for parsing system list
+	(*sinitial)->scompress = Hkey;//set hash before new node creation
+	sysmap_dt * newsnode = (sysmap_dt *) malloc(sizeof(sysmap_dt));
+	(*sinitial)->s_nextnode=newsnode;
+	newsnode->s_prevnode=(*sinitial);
+	for(int i = 0;i< 6;i++)//array scrub of trash data
+	{
+		for(int j = 0;j<6;j++)
+		{
+			newsnode->sysmapc[i][j] = 0;
+		}
+	}
+	(*sinitial) = newsnode;
+}
+
+int sysnodedel(int16_t findme)//running error present here
+{
+	sysmap_dt * deleteme = sysorigin;
+	sysmap_dt * nextme;
+	sysmap_dt * prevme;
+	int found = 0;
+	while(deleteme->scompress != findme && deleteme->s_nextnode != NULL)// finding target node within list
+	{
+		deleteme = deleteme->s_nextnode;
+		nextme = deleteme->s_nextnode;
+		prevme = deleteme->s_prevnode;
+		found = 1;
+	}
+	if(found == 0)
+	{return 1;}
+	if(nextme == NULL && prevme != NULL)//turning trailer+1 node into trailer node
+	{
+		prevme->s_nextnode = NULL;
+	}
+	if(prevme == NULL && nextme != NULL)//header+1 becoming header node
+	{
+		nextme->s_prevnode = NULL;
+	}
+	if(deleteme->s_nextnode == NULL && deleteme->s_prevnode == NULL)//deleting this will delete sys origin, don't delete, set to zero return value is 3, incase something requires feedback on what happened in teh delete
+	{
+		sysorigin->scompress = -1; //-1 in the compressed value will be used to determine that the node is clean
+		for(int i = 0; i < 6;i++)
+			{for(int j = 0; j < 6; j++)
+				{sysorigin->sysmapc[i][j] = 0;}}
+		for(int i = 0; i < 255; i++)
+			{sysorigin->s_descrip[i] = 0;}
+		sysorigin->s_nextnode = NULL;		
+		sysorigin->s_prevnode = NULL;
+		return 3;
+	}
+	if(deleteme->s_nextnode != NULL && deleteme->s_prevnode != NULL)//linking adjacent nodes
+	{
+		nextme->s_prevnode = prevme;
+		prevme->s_nextnode = nextme;
+	}
+	free(deleteme);//deleting node
+}
+
+int16_t coordcompress16(int x, int y) // a single function that compresses two variables into a single variable while retaining data integrity
+{
+	int16_t returnvalue = 0;
+	returnvalue += x;
+	returnvalue = returnvalue<< 8;
+	returnvalue += y;
+	return returnvalue;
+}
+
+int32_t coordcompress32(int w, int x, int y, int z) // functions like the 16 bit version, w,x are the x,y coordinates in the system map, y,z are the coordinates for the zone
+{
+	int32_t returnvalue = 0;
+	returnvalue += coordcompress16(w,x);
+	returnvalue = returnvalue << 16;
+	returnvalue += coordcompress16(y,z);
+	return returnvalue;
 }
 
 //---------------|| 							 							||--------------------------------------
@@ -313,6 +481,10 @@ void originprint()
 		printf("\n");
 	}
 	printf("g_nextnode is: %p\ng_prevnode is: %p\nsysorigin array dump is:\n",galorigin->g_nextnode,galorigin->g_prevnode);
+	// this prevents the final node(blank from appearing, a do while would cause it to appear
+	do {
+	printf("\nsysorigin's Scompress is: %d\n",sysorigin->scompress);
+	printf("s_nextnode is: %p\ns_prevnode is: %p\nsysorigin address is %p\n",sysorigin->s_nextnode,sysorigin->s_prevnode,sysorigin);
 	for(int i = 0;i< 6;i++)
 	{
 		for(int j = 0;j<6;j++)
@@ -321,8 +493,19 @@ void originprint()
 		}
 		printf("\n");
 	}
-	printf("s_nextnode is: %p\ns_prevnode is: %p\nzorigin array dump is:\n",sysorigin->s_nextnode,sysorigin->s_prevnode);
-	printf("\nzcoord(x,y) is: (%d,%d)\n",zorigin->z_coord[0],zorigin->z_coord[1]);
+		sysorigin = sysorigin->s_nextnode;
+	}while(sysorigin->s_nextnode != NULL);//these are seperated becaus ethe final node has a null pointer and is not run within this loop
+	printf("\nsysorigin's Scompress is: %d\n",sysorigin->scompress);
+	printf("s_nextnode is: %p\ns_prevnode is: %p\nsysorigin address is %p\n",sysorigin->s_nextnode,sysorigin->s_prevnode,sysorigin);
+	for(int i = 0;i< 6;i++)
+	{
+		for(int j = 0;j<6;j++)
+		{
+			printf("%c ",sysorigin->sysmapc[i][j]);
+		}
+		printf("\n");
+	}
+	printf("zorigin array dump is:\n");
 	printf("z_nextnode is: %p\nz_prevnode is: %p\n",zorigin->z_nextnode,zorigin->z_prevnode);
 	system("pause");
 }
